aa8f9d056a413d1f61e3c0b81e43d91d5a30a9a0
divinejoshua <divineuche399@gmail.com>
2024-08-10 23:35:34 +0100

    Added mermaid to next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       {/* eslint-disable-next-line @next/next/inline-script-id */}
+       <Script
+          type="module"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";
+            mermaid.contentLoaded();
+          `,
+          }}
+        />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,125 @@
+"use client"
+import mermaid from "mermaid";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+
+  const [mermaidChart, setmermaidChart] = useState("")
+
+  const mermaidRef = useRef(null);
+
+  const setValueOne = () =>{
+    setmermaidChart(`
+      sequenceDiagram
+        participant Customer
+        participant BankingApp
+        participant AccountService
+        participant TransactionService
+        participant NotificationService
+  
+        Customer->>BankingApp: Initiates Transaction (e.g., Transfer Money)
+        BankingApp->>AccountService: Validate Account and Balance
+        AccountService-->>BankingApp: Account Validated
+  
+        BankingApp->>TransactionService: Create Transaction
+        TransactionService-->>BankingApp: Transaction Created
+        
+        BankingApp->>AccountService: Deduct Amount from Account
+        AccountService-->>BankingApp: Amount Deducted
+        
+        BankingApp->>NotificationService: Send Transaction Notification
+        NotificationService-->>Customer: Notification Sent
+        
+        BankingApp-->>Customer: Transaction Success Messages
+        `
+    )
+
+
+    // // Get the element by ID
+    // var mermaidElement : any = document.getElementsByClassName('mermaid');
+
+    // console.log(document.querySelector('mermaid'))
+
+    // // Set the inner HTML of the element
+    // mermaidElement.innerHTML = mermaidChart;
+
+    // // Remove the 'data-processed' attribute
+    // mermaidElement.removeAttribute('data-processed');
+
+    // // Initialize Mermaid
+    // mermaid.init(undefined, mermaidElement);
+  }
+
+
+
+  const setValueTwo = () =>{
+    setmermaidChart(`
+     flowchart TD
+        A[Start] --> B[Do you have an idea?]
+        B -->|Yes| C[Validate the idea]
+        B -->|No| D[Brainstorm new ideas]
+        C --> E[Create a prototype]
+        D --> B
+        E --> F[Gather feedback]
+        F --> G[Is the feedback positive?]
+        G -->|Yes| H[Develop the product]
+        G -->|No| I[Improve the prototype]
+        I --> E
+        H --> J[Launch the product]
+        J --> K[Market the product]
+        K --> L[Product succeeds?]
+        L -->|Yes| M[Scale the product]
+        L -->|No| N[Pivot or discontinue]
+        N --> D
+        M --> O[Celebrate success!]
+      `
+    )}
+
+  useEffect(() => {
+    // setValue()
+
+    setTimeout(
+      function() {
+        setValueOne()
+      }, 2000);
+
+      setTimeout(
+        function() {
+          setValueTwo()
+        }, 5000);
+    
+  }, [])
+
+
+
+  useEffect(() => {
+    console.log("should be here")
+    // Ensure Mermaid is initialized when the component mounts or graphDefinition updates
+    if (mermaidRef.current) {
+        mermaidRef.current.innerHTML = mermaidChart;
+        mermaidRef.current.removeAttribute('data-processed');
+        mermaid.init(undefined, mermaidRef.current);
+    }
+}, [mermaidChart]); // Run the effect when graphDefinition changes
+
+  
+
+  const PreviewMermaidCode = ({ value } : { value : string}) => {
+
+    return (
+      value && <div ref={mermaidRef}></div>
+      // value &&
+      // <div className="mermaid">
+      //   {value}
+      // </div>
+    );
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <PreviewMermaidCode value={mermaidChart} />
     </main>
   );
 }
